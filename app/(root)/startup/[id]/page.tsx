036bf2239247263b512e070d90cdd620f77ea7f5
Krishn Parasar <76171905+Krishn1412@users.noreply.github.com>
2025-03-29 20:56:40 +0530

    New Feature

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -9,23 +9,19 @@ import { formatDate } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
 
-// import markdownit from "markdown-it";
-// import { Skeleton } from "@/components/ui/skeleton";
-// import View from "@/components/View";
+import { Skeleton } from "@/components/ui/skeleton";
+import View from "@/components/View";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
-// const md = markdownit();
+
 
 export const experimental_ppr = true;
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const [post, { select: editorPosts }] = await Promise.all([
-    client.fetch(STARTUP_BY_ID_QUERY, { id }),
-    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
-      slug: "editor-picks-new",
-    }),
+  const [post] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id })
   ]);
 
   if (!post) return notFound();
@@ -86,7 +82,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <hr className="divider" />
 
-        {editorPosts?.length > 0 && (
+        {/* {editorPosts?.length > 0 && (
           <div className="max-w-4xl mx-auto">
             <p className="text-30-semibold">Editor Picks</p>
 
@@ -96,11 +92,11 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
               ))}
             </ul>
           </div>
-        )}
+        )} */}
 
-        {/* <Suspense fallback={<Skeleton className="view_skeleton" />}>
+        <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
-        </Suspense> */}
+        </Suspense>
       </section>
     </>
   );
